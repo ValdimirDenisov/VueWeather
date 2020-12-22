1acf4eecd4e48e43668d54053402a5e0810a436f
Vladimir <vova_denisov_2003@mail.ru>
2020-12-22 21:12:49 +0300

    Fix and finish Ajax

--- vue_weather/src/App.vue
+++ vue_weather/src/App.vue
@@ -5,8 +5,8 @@
       <div class="flex-break"></div>
       <Humidity v-bind:humidity="humidity"/>
       <div class="vertical-divider"></div>
-      <Wind v-bind:wind_speed='wind' />
-
+      <Wind v-bind:wind_speed='wind'/>
+      {{getWeather()}}
     </div>
   </div>
 </template>
@@ -30,17 +30,67 @@ export default {
     Temperature,
     Humidity,
     Wind
+  },
+  methods: {
+    getWeather() {
+      let api_w = {
+        coordinates: '56.011759, 37.859407',
+        a_id: '082dcc0a',
+        a_key: '0e1fd3c764d42e908f2ccfacbbeca952'
+      }
+      let url = 'http://api.weatherunlocked.com/api/current/'+ api_w['coordinates'] + '?app_id=' + api_w['a_id'] + '&app_key=' + api_w['a_key'];
+      this.axios.get(url)
+        .then((response) => {
+          let arr = response['data'];
+          this.wind = arr['windspd_ms'];
+          this.temperatureReal = arr['temp_c'];
+          this.temperatureFeel = arr['feelslike_c'];
+          this.humidity = arr['humid_pct'];
+        });
+    }
+  },
+  mounted() {
+    setInterval(this.getWeather(), 10000);
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  .app {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background: rgb(244, 123, 186);
+    background: linear-gradient(-25deg, rgba(244, 123, 186, 0.5) 20%, rgba(90, 95, 245, 0.5) 80%);
+  }
+  .informer-container {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    width: 400px;
+    padding: 30px;
+    background-image: url('assets/img/forest.png');
+    background-size: cover;
+    background-position: center center;
+    border-radius: 20px;
+    box-shadow: 0px 0px 10px 0px rgba(50, 50, 50, 0.75);
+  }
+  
+  .flex-break{
+    flex-basis: 100%;
+    height: 0;
+  }
+  .vertical-divider{
+    height: 70px;
+    width: 1px;
+    background-color: #fff;
+    vertical-align: middle;
+    display: table-cell;
+  }
 </style>
--- vue_weather/src/components/wind.vue
+++ vue_weather/src/components/wind.vue
@@ -1,27 +1,31 @@
 <template>
-    <div class="wind" v-on:click="reload_data()">
-        <img src="img/wind.png"> {{wind_speed}} <small>м/с</small>
-    </div>
+  <div class="wind" v-on:click="reload_data()">
+    <img src="../assets/img/wind.png"> {{wind_speed}} <small>м/с</small>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'Wind',
-  props: ['wind_speed'],
-  methods: {
-     reload_data() {
-      alert(wind_speed);
-      let xhr = new XMLHttpRequest;
-      xhr.open('GET', 'http://37.77.104.246/api/weather/wind.php', true);
-      xhr.send();
-      xhr.addEventListener('readystatechange', function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-          this.wind_speed = xhr.responseText;
-        } else {
-          this.wind_speed = 0;
-        }
-      });
-    }
+  export default {
+    name: 'Wind',
+    props: ['wind_speed'],
   }
-}
 </script>
+
+<style>
+  .wind {
+    font-family: 'Arial', sans-serif;
+    font-size: 40px;
+    font-weight: 400;
+    color: #fff;
+    position: relative;
+    display: inline-block;
+    line-height: 0.9;
+    text-shadow: 2px 2px 2px #444;
+    margin: 15px;
+  }
+  .wind img {
+    width: 30px;
+    margin-right: -5px;
+    vertical-align: middle;
+  }
+</style>
